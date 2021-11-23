81135ebd1cae452d7598b9abccf978339172d415
Muhamad Fahmi Rahmatullah <mfr@MacBook-Pro-M1.local>
2021-11-23 14:53:15 +0700

    Handle error/validation on form and setup title dinamis each page

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
+
 // import mongoose
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/db_bwa_staycation", {
@@ -29,6 +32,15 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
+app.use(flash());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
