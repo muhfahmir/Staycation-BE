fd3a8125aa68b81a976883045477070b4ab9d62e
Muhamad Fahmi Rahmatullah <fahmi.rahmat07@gmail.com>
2021-12-03 23:35:14 +0700

    adding cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+const cors = require("cors");
 
 // import mongoose
 const mongoose = require("mongoose");
@@ -46,6 +47,7 @@ app.use(
     },
   })
 );
+app.use(cors());
 app.use(flash());
 app.use(logger("dev"));
 app.use(express.json());
